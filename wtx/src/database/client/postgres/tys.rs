943dfc242f594bb103726c08cea38e02295690d2
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2025-02-05 20:56:28 +0800

    chore: dbg for local usage

--- wtx/src/database/client/postgres/tys.rs
+++ wtx/src/database/client/postgres/tys.rs
@@ -386,11 +386,11 @@ mod collections {
   {
     #[inline]
     fn encode(&self, ev: &mut EncodeValue<'_, '_>) -> Result<(), E> {/*
-      ev.fbw().extend_from_slice(&1_u32.to_be_bytes())?;
       */
 
       std::dbg!(&ev);
 
+      ev.fbw().extend_from_slice(&1_u32.to_be_bytes())?;
       ev.fbw().extend_from_slice(&0_u32.to_be_bytes())?;
       ev.fbw().extend_from_slice(&25_u32.to_be_bytes())?;
       ev.fbw().extend_from_slice(&self.len().to_be_bytes())?;
